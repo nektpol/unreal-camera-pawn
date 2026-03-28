5f819e00462420b0ade0c5196493aec0f4fb6661
nektpol <36765011+nektpol@users.noreply.github.com>
2026-03-28 13:20:40 +0200

    Create input action move

--- Plugins/TopDownCameraPawn/Source/TopDownCameraPawn/Private/TDCP_CameraPawn.cpp
+++ Plugins/TopDownCameraPawn/Source/TopDownCameraPawn/Private/TDCP_CameraPawn.cpp
@@ -3,21 +3,26 @@
 
 #include "TDCP_CameraPawn.h"
 
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 
 ATDCP_CameraPawn::ATDCP_CameraPawn()
 {
 	PrimaryActorTick.bCanEverTick = true;
+	
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	SetRootComponent(Root);
 
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->SetupAttachment(Root);
 	CameraBoom->bDoCollisionTest = false;
 	CameraBoom->TargetArmLength = 1200.f;
 	CameraBoom->SetRelativeRotation(FRotator(-60.f, 0.f, 0.f));
 	
 	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
-	Camera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	Camera->SetupAttachment(CameraBoom);
 }
 
 void ATDCP_CameraPawn::OnConstruction(const FTransform& Transform)
@@ -26,6 +31,63 @@ void ATDCP_CameraPawn::OnConstruction(const FTransform& Transform)
 	
 	CameraBoom->TargetArmLength = DefaultArmLength;
 	CameraBoom->SetRelativeRotation(FRotator(DefaultPitch, 0.f, 0.f));
+}
+
+void ATDCP_CameraPawn::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+	
+	if (!CameraMovementMappingContext)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("CameraMovementMappingContext is not set!"));
+		return;
+	}
+
+	if (APlayerController* PC = Cast<APlayerController>(NewController))
+	{
+		if (ULocalPlayer* LP = PC->GetLocalPlayer())
+		{
+			if (UEnhancedInputLocalPlayerSubsystem* Subsystem =
+				LP->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+			{
+				Subsystem->AddMappingContext(CameraMovementMappingContext, 0);
+			}
+		}
+	}
+}
+
+void ATDCP_CameraPawn::UnPossessed()
+{
+	Super::UnPossessed();
+	
+	if (!CameraMovementMappingContext)
+	{
+		return;
+	}
+
+	if (APlayerController* PC = Cast<APlayerController>(GetController()))
+	{
+		if (ULocalPlayer* LP = PC->GetLocalPlayer())
+		{
+			if (UEnhancedInputLocalPlayerSubsystem* Subsystem =
+				LP->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+			{
+				Subsystem->RemoveMappingContext(CameraMovementMappingContext);
+			}
+		}
+	}
+}
+
+void ATDCP_CameraPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	if (UEnhancedInputComponent* EIC = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EIC->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ATDCP_CameraPawn::HandleMoveInput);
+		EIC->BindAction(MoveAction, ETriggerEvent::Completed, this, &ATDCP_CameraPawn::HandleMoveInput);
+	}
+
 }
 
 void ATDCP_CameraPawn::BeginPlay()
@@ -37,12 +99,49 @@ void ATDCP_CameraPawn::BeginPlay()
 void ATDCP_CameraPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	
+	UpdateMovement(DeltaTime);
 
 }
 
-void ATDCP_CameraPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void ATDCP_CameraPawn::UpdateMovement(const float& DeltaTime)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	const float InterpSpeed = TargetVelocity.IsNearlyZero() ? Deceleration : Acceleration;
 
+	CurrentVelocity = FMath::VInterpTo(CurrentVelocity,	TargetVelocity,	DeltaTime, InterpSpeed);
+
+	// Prevent micro jitter
+	if (CurrentVelocity.SizeSquared() < 1.f)
+	{
+		CurrentVelocity = FVector::ZeroVector;
+	}
+
+	AddActorWorldOffset(CurrentVelocity * DeltaTime, true);
 }
+
+void ATDCP_CameraPawn::HandleMoveInput(const FInputActionValue& Value)
+{
+	const FVector2D Input = Value.Get<FVector2D>();
+
+	if (!Controller) return;
+
+	if (Input.IsNearlyZero())
+	{
+		TargetVelocity = FVector::ZeroVector;
+		return;
+	}
+
+	const FRotator YawRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+
+	const FVector Forward = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector Right   = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	const FVector Direction = (Forward * Input.Y + Right * Input.X).GetClampedToMaxSize(1.f);
+
+	TargetVelocity = Direction * MoveSpeed;
+}
+
+
+
+
 
--- Plugins/TopDownCameraPawn/Source/TopDownCameraPawn/Public/TDCP_CameraPawn.h
+++ Plugins/TopDownCameraPawn/Source/TopDownCameraPawn/Public/TDCP_CameraPawn.h
@@ -6,6 +6,9 @@
 #include "GameFramework/Pawn.h"
 #include "TDCP_CameraPawn.generated.h"
 
+struct FInputActionValue;
+class UInputMappingContext;
+class UInputAction;
 class UCameraComponent;
 class USpringArmComponent;
 
@@ -17,6 +20,8 @@ class TOPDOWNCAMERAPAWN_API ATDCP_CameraPawn : public APawn
 public:
 	ATDCP_CameraPawn();
 	virtual void OnConstruction(const FTransform& Transform) override;
+	virtual void PossessedBy(AController* NewController) override;
+	virtual void UnPossessed() override;
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
@@ -25,6 +30,9 @@ protected:
 
 
 private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USceneComponent> Root;
+	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<USpringArmComponent> CameraBoom;
 	
@@ -35,4 +43,33 @@ private:
 	float DefaultArmLength = 1200.f;
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "TDCP|Settings", meta = (AllowPrivateAccess = "true"))
 	float DefaultPitch = -60.f;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "TDCP|Settings", meta = (AllowPrivateAccess = "true"))
+	float MoveSpeed = 1500.f;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "TDCP|Settings", meta = (AllowPrivateAccess = "true"))
+	float Acceleration = 5.f;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "TDCP|Settings", meta = (AllowPrivateAccess = "true"))
+	float Deceleration = 8.f;
+	
+	
+	FVector CurrentVelocity = FVector::ZeroVector;
+	FVector TargetVelocity = FVector::ZeroVector;
+	
+	void UpdateMovement(const float& DeltaTime);
+	
+	
+#pragma region Input
+
+protected:
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "TDCP|Input")
+	UInputMappingContext* CameraMovementMappingContext;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "TDCP|Input")
+	UInputAction* MoveAction;
+	
+private:
+	void HandleMoveInput(const FInputActionValue& Value);
+
+
+#pragma endregion 
 };
